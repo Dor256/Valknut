339ad34f1c307e6e5a469e833d68941311f6e6d7
Dor <dorjp6@gmail.com>
2020-02-26 20:54:31 +0200

    Add eslint rules, new button styles, and change stories to use storyOf api

--- src/Button/index.stories.tsx
+++ src/Button/index.stories.tsx
@@ -1,18 +1,18 @@
 import React from "react";
+import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
 import { Button } from '.';
 
-export default {
-  title: "Button"
-};
-
-export const Regular = () => <Button>Regular</Button>;
-
-export const Blue = () => <Button color='blue'>Blue</Button>;
-
-export const Green = () => <Button color='green'>Green</Button>;
-
-export const Yellow = () => <Button color='yellow'>Yellow</Button>;
-
-export const Red = () => <Button color='red'>Red</Button>;
-
-export const Clickable = () => <Button onClick={() => alert("It Clicks!")}>Click Me!</Button>;
+storiesOf('Button', module)
+  .add('Reguler', () => <Button>Regular</Button>)
+  .add('Green', () => <Button color='green'>Green</Button>)
+  .add('Yellow', () => <Button color='yellow'>Yellow</Button>)
+  .add('Red', () => <Button color='red'>Red</Button>)
+  .add('Black', () => <Button color='black'>Black</Button>)
+  .add('Purple', () => <Button color='purple'>Purple</Button>)
+  .add('Violet', () => <Button color='violet'>Violet</Button>)
+  .add('Teal', () => <Button color='teal'>Teal</Button>)
+  .add('Orange', () => <Button color='orange'>Orange</Button>)
+  .add('Brown', () => <Button color='brown'>Brown</Button>)
+  .add('Inverse', () => <Button color='blue' inverted>Inverse Blue</Button>)
+  .add('Clickable', () => <Button onClick={action('clicked')}>Click Me!</Button>);
--- src/Button/index.tsx
+++ src/Button/index.tsx
@@ -3,12 +3,13 @@ import './_style.scss';
 
 export type ButtonProps = {
     children?: React.ReactNode,
-    color?: 'blue' | 'green' | 'yellow' | 'red';
+    color?: 'blue' | 'green' | 'yellow' | 'red' | 'black' | 'purple' | 'violet' | 'teal' | 'orange' | 'brown',
+    inverted?: boolean
 } & React.DetailedHTMLProps<React.HTMLAttributes<HTMLButtonElement>, HTMLButtonElement>
 
 export const Button = (props: ButtonProps) => {
-    const { children, className, color } = props;
-    const cssClass = `${color ? ` ${color} ` : ''}${className ? ` ${className}` : ''}`;
+    const { children, className, color, inverted, ...buttonProps } = props;
+    const cssClass = `${inverted ? ' inverted' : ''}${color ? ` ${color} ` : ''}${className ? `${className}` : ''}`;
 
-    return <button {...props} className={`val${cssClass}`}>{children}</button>;
-}
+    return <button {...buttonProps} className={`val${cssClass}`}>{children}</button>;
+};
