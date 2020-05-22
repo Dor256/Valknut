60e234ee4f84b37aec6b56e18d5b27030deebb73
Dor <dorjp6@gmail.com>
2020-05-22 09:48:43 +0300

    Remove dist folder

--- dist/Container/index.d.ts
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import './_style.scss';
-export declare type ContainerProps = {
-    children?: React.ReactNode;
-    flex?: boolean;
-} & React.DetailedHTMLProps<React.HTMLAttributes<HTMLDivElement>, HTMLDivElement>;
-export declare const Container: (props: ContainerProps) => JSX.Element;
--- dist/index.d.ts
+++
@@ -1,3 +0,0 @@
-export { Button } from './Button';
-export { Container } from './Container';
-export { Navbar } from './Navbar';
